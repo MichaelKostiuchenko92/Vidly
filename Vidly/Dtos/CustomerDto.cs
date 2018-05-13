8faf451a9322a41b3fc1ea27d8f6faf7e93c5486
MichaelKostiuchenko92 <justrumpled@gmail.com>
2018-05-13 12:36:45 +0300

    APIs asd DTOs added

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter Name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? birthDate { get; set; }
+
+      
+        public int MembershipTypeId { get; set; }
+    }
+}
